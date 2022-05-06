b441aec177a07173300f25a6c2e6dd352b102514
Arelial ID <98020400+4relial@users.noreply.github.com>
2022-05-06 07:59:13 +0000

    just update

--- config/configFile.js
+++ config/configFile.js
@@ -9,7 +9,7 @@ exports.info = {
         numero: ["6289515275674@s.whatsapp.net"]
     },
     premium: {
-        number: ["6289515457047@s.whatsapp.net","62895361892629@s.whatsapp.net","6285280341639@s.whatsapp.net"]
+        number: ["6289515457047@s.whatsapp.net","6289515275674@s.whatsapp.net","62895361892629@s.whatsapp.net","6285280341639@s.whatsapp.net"]
     },
     grupo: ""
 }
