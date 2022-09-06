f71330723a6eef0a3c74a7d13eb11f909d47c366
M Ilham Ari Pahmi <98020400+4relial@users.noreply.github.com>
2022-09-06 18:06:50 +0700

    Fix bug

--- index.js
+++ index.js
@@ -6,47 +6,42 @@ const fs = require('fs')
 const conf = require('./config/configFile').info
 const qrcode = require("qr-image");
 const logger = MAIN_LOGGER.child({})
-const { delGrup, getgroup } = require('./lib/DBUser')
+const { global } = require('./lib/global')
 
 
-   exports.Start = async() => {
+async function startSock(anu) {
 try {
-        //Retry Handler
-        const { state, saveCreds } = await useMultiFileAuthState('.state')
-        // fetch latest version of WA Web
-        const { version, isLatest } = await fetchLatestBaileysVersion()
-        console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+    const { state, saveCreds } = await useMultiFileAuthState('.state')
+    // fetch latest version of WA Web
+    const { version, isLatest } = await fetchLatestBaileysVersion()
+    console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+    const store = makeInMemoryStore({})
+    store.readFromFile('./baileys_store.json')
+    setInterval(() => {
+        store.writeToFile('./baileys_store.json')
+    }, 10_000)
 
-  async function startSock(){
-    
-        const store = makeInMemoryStore({})
-        store.readFromFile('./baileys_store.json')
-        setInterval(() => {
-            store.writeToFile('./baileys_store.json')
-        }, 10_000)
-
-        const sock = makeWASocket({
-            version,
-            logger: Pino({ level: "silent" }),
-            printQRInTerminal: true,
-            defaultQueryTimeoutMs: undefined,
-            browser: ['Megumi MD','Safari','1.0.0'],
-            auth: state,
-            // implement to handle retries
-            getMessage: async key => {
-                if (store) {
-                    const msg = await store.loadMessage(key.remoteJid, key.id, undefined)
-                    return msg?.message || undefined
-                }
-
-                // only if store is present
-                return {
-                    conversation: 'Terjadi Kesalahan, Ulangi Command!'
-                }
+    const sock = makeWASocket({
+        version,
+        logger: Pino({ level: "silent" }),
+        printQRInTerminal: true,
+        browser: ['Megumi MD', 'Safari', '9.4.5'],
+        auth: state,
+        // implement to handle retries
+        getMessage: async key => {
+            if (store) {
+                const msg = await store.loadMessage(key.remoteJid, key.id, undefined)
+                return msg?.message || undefined
             }
-        })
 
-        store.bind(sock.ev)
+            // only if store is present
+            return {
+                conversation: 'Terjadi Kesalahan, Ulangi Command!'
+            }
+        }
+    })
+
+    store.bind(sock.ev)
 
         sock.ev.on('connection.update', (update) => {
 
@@ -83,7 +78,31 @@ try {
         sock.ev.on('creds.update', saveCreds)
 
 
-        sock.ev.on('messages.upsert', m => core(sock, m))
+        sock.ev.on('messages.upsert', async (m) => {
+
+        try {
+
+            mei = global.d.verificarMei(m)
+            if (!mei) return
+
+            const from = mei.key.remoteJid
+            var group = from.endsWith('@g.us')
+            let sender = group ? mei.key.participant : mei.key.remoteJid
+            let fromPC = group ? sender.includes(':') ? true : false : false
+            sender = fromPC ? sender.split(':')[0] + '@s.whatsapp.net' : sender
+
+            await sock.readMessages([mei.key])
+            await core(sock, m)
+        } catch (e) {
+            if (e.toString().includes('this.isZero')) return
+            var today = new Date();
+            var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear()
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var dateTime = date + '|' + time;
+            console.log(`${dateTime}>>>> `, e, '<<<<')
+        }
+    })
+    
         sock.ev.on('group-participants.update', async (anu) => {
           if(!anu.participants.includes(conf.bot.number)) {
             console.log(anu)
@@ -134,9 +153,53 @@ try {
             }
         }
         })
-  }
-  startSock()
 } catch (e) { reject(e) }
     }
-    
 
+//web
+
+const rimraf = require('rimraf')
+
+startSock()
+
+rimraf('./assets/downloads/*', function () {
+  console.log('Data Dihapus!') 
+})
+
+//WEB SERVER
+
+const express = require("express")
+const cors = require("cors")
+const request = require("request")
+const got = require("got")
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    if (fs.existsSync('./img.png')) {
+        res.sendFile(__dirname + '/img.png')
+    } else {
+        res.sendFile(__dirname + '/megumi.html')
+    }
+
+});
+
+app.get("/ping", (req, res) => {
+        res.sendFile(__dirname + '/ping.html')
+});
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log("Megumi Server is Active in Port: " + PORT);
+});
+
+
+          //WEB SERVER
