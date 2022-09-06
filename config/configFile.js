5fa8c0204dcd3e0ec0240f7d8403e82c433327d5
M Ilham Ari Pahmi <98020400+4relial@users.noreply.github.com>
2022-09-06 19:13:05 +0700

    Fix BUG Numero

--- config/configFile.js
+++ config/configFile.js
@@ -1,15 +1,17 @@
 
 exports.info = {
-    prefix: '#',
-	prefix2: '.',
-    owner: {
-        nome: "Tomoya Aki",
-        numero: ["6289515275674@s.whatsapp.net"]
-    },
-    grupo: ""
-}
-
-exports.connectionFileName = () => {
-    var path = './connect.json'
-    return path
-}
+    prefix: '.',
+      prefix2: '#',
+      owner: {
+          nome: "4relial",
+          number: "6289515275674@s.whatsapp.net",
+          numero: ["6289515275674@s.whatsapp.net"]
+      }, 
+      bot: {
+          name: "Megumi Katou",
+          number: "6283157447725@s.whatsapp.net"
+      },
+      grupo: ""
+  }
+  
+  exports.connectionFileName = './connect.json'
