9585ce5dbc271b99bcdbcf1366f99c2e57a1e613
M Ilham Ari Pahmi <98020400+4relial@users.noreply.github.com>
2023-02-27 11:57:47 +0000

    change bot Name in Config

--- config/configFile.js
+++ config/configFile.js
@@ -1,15 +1,17 @@
 exports.info = {
-    prefix: '.',
-      prefix2: '-',
-      owner: {
-          nome: "4relial",
-          number: "Your Owner number",
-          numero: ["Your Owner Number"]
-      }, 
-      bot: {
-          name: "Megumi Katou",
-          number: "Your Bot Number"
-      },
-      grupo: "",
+  prefix: '.',
+  prefix2: '-',
+  owner: {
+    nome: "4relial",
+    number: "6289515275674@s.whatsapp.net",
+    numero: ["6289515275674@s.whatsapp.net"] //change to your owner number
+  },
+  bot: {
+    name: "Megumi Katou",
+    number: "6283157447725@s.whatsapp.net", //change to your bot number
+    shortName: "Megumi",
+    bigName: "𝕄𝔼𝔾𝕌𝕄𝕀 𝕂𝔸𝕋𝕆𝕌"
+  },
+  grupo: "",
   apikeyRemoveBg: " Your ApiKey"
-  }
+}
--- start.js
+++ start.js
@@ -14,52 +14,60 @@ app.use(express.json());
 
 app.get('/gambar', (req, res) => {
   const path = __dirname + '/img.png';
-
+  const path2 = __dirname + '/done.jpg';
   res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-
-  res.sendFile(path, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+  if (fs.existsSync('./img.png')) {
+    res.sendFile(path, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  } else {
+    res.sendFile(path2, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  }
 });
 
 app.get("/", (req, res) => {
-    if (fs.existsSync('./img.png')) {
-       res.sendFile(__dirname + '/qr.html')
-    } else {
-        res.sendFile(__dirname + '/megumi.html')
-    }
+  if (fs.existsSync('./img.png')) {
+    res.sendFile(__dirname + '/qr.html')
+  } else {
+    res.sendFile(__dirname + '/megumi.html')
+  }
 
 });
 
-app.get('/ping',function(req, res) {
-    const ipAddress = req.socket.remoteAddress;
-    res.send(ipAddress);
+app.get('/ping', function(req, res) {
+  const ipAddress = req.socket.remoteAddress;
+  res.send(ipAddress);
 });
 
 
 app.get("/restart", (req, res) => {
-    try {
-        res.sendFile(__dirname + '/ping.html')
-    } finally {
-        var sys = require('sys')
-        var exec = require('child_process').exec;
+  try {
+    res.sendFile(__dirname + '/ping.html')
+  } finally {
+    var sys = require('sys')
+    var exec = require('child_process').exec;
 
-        function puts(error, stdout, stderr) { sys.puts(stdout) }
-        exec("kill 1", function (err, stdout, stderr) {
-            console.log(stdout);
-        })
-    }
+    function puts(error, stdout, stderr) { sys.puts(stdout) }
+    exec("kill 1", function(err, stdout, stderr) {
+      console.log(stdout);
+    })
+  }
 })
 
 app.get("/del", (req, res) => {
-    if (fs.existsSync('./multi_state/store.json')) {
-        fs.unlinkSync('./multi_state/store.json')
-    }
+  if (fs.existsSync('./multi_state/store.json')) {
+    fs.unlinkSync('./multi_state/store.json')
+  }
 
-    res.sendFile(__dirname + '/ping.html')
+  res.sendFile(__dirname + '/ping.html')
 
 })
 
@@ -68,5 +76,5 @@ startSock()
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log("Megumi Server is Active in Port: " + PORT);
+  console.log("Megumi Server is Active in Port: " + PORT);
 });
